4768431d8002d12bd67b7fc784b404e4b7bd5d1b
Lucas Tavares <lucastavarestt@gmail.com>
2025-03-12 15:59:41 -0300

    .

--- public/widget.js
+++ public/widget.js
@@ -9,8 +9,8 @@
     const srcIframe = src.slice(0, src.lastIndexOf("/"));
     const iFrame = document.createElement("iframe");
     iFrame.id = iframeWidget;
-    iFrame.style.width = "300px";
-    iFrame.style.height = "400px";
+    iFrame.style.width = "100%";
+    iFrame.style.height = "220px";
     iFrame.style.border = "none";
     iFrame.src = srcIframe;
     document.body.appendChild(iFrame);
