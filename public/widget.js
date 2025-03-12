50cbc90151f21aae9fe4f7e71094b1d5049d1bdf
Lucas Tavares <lucastavarestt@gmail.com>
2025-03-12 15:52:43 -0300

    .

---
+++ public/widget.js
@@ -0,0 +1,18 @@
+"use strict";
+(() => {
+  // src/widgetScript.ts
+  (function() {
+    const iframeWidget = "iframeWidget";
+    const doc = document.currentScript;
+    if (!doc) return;
+    const src = doc.getAttribute("src");
+    const srcIframe = src.slice(0, src.lastIndexOf("/")) + "/widget";
+    const iFrame = document.createElement("iframe");
+    iFrame.id = iframeWidget;
+    iFrame.style.width = "300px";
+    iFrame.style.height = "400px";
+    iFrame.style.border = "none";
+    iFrame.src = srcIframe;
+    document.body.appendChild(iFrame);
+  })();
+})();
